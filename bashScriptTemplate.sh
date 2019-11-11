20cdff16d39b2c15cd4b21133678ae273c797ff3
Christian Imhorst <christian.imhorst@gmail.com>
2019-11-11 15:57:12 +0100

    Update bashScriptTemplate.sh

--- bashScriptTemplate.sh
+++ bashScriptTemplate.sh
@@ -25,36 +25,47 @@ SOURCE=""
 #   - some FIX or TODO here
 #
 # -- MAIN FUNCTIONS ------------------------------------------------------------
-Get_help() { # Help text
 
+usage() { echo "Usage: $0 [-h] [-v] [-s <string>]" 1>&2; exit 1; }
+
+help() { # Help text
+echo $(usage)
 cat <<EOF
-Usage: $0 [options]
 Example:
 
 Options:
     -h, --help                  print this help
     -v, --version               print script version
+    -s, --say <string>          say something
 EOF
 }
 
-Get_version() { # Script Version
+version() { # Script Version
     echo "$PROGRAM: v$VERSION"
+}
+
+say() {
+    echo "You said: $1"
 }
 
 # -- RUN SCRIPT ----------------------------------------------------------------
 
+if [ -z $1 ]; then usage; fi
+
 # Parsing bash script options with getopt
-options=$(getopt -o hv -l help,version -- "$@")
+options=$(getopt -o hvs: -l help,version,say: -- "$@")
 if [ $? != 0 ] ; then echo "Terminating..." >&2; exit 1; fi
 
 set -- $options
 while [ $# -gt 0 ]
 do
    case $1 in
-        -h|--help) Get_help; exit;;
-        -v|--version) Get_version; exit;;
-        (--) shift; break;;
+        -h|--help) help; exit;;
+        -v|--version) version; exit;;
+        -s|--say) say ${2//\'/}; exit;;
+        (--) break;;
         (-*) echo "$0: error - unrecognized option $1" 1>&2; exit 1;;
         (*)  break;;
     esac
 done
+shift $((OPTIND -1))
